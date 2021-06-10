d14afbea2a70f2822751b476b1bb630d0694f55e
DOG007ua <dog007ua@gmail.com>
2021-06-10 08:59:34 +0300

    Start create guns

---
+++ Assets/Scripts/Classes/Gun/IGun.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGun 
+{
+    InputPropertyGun Property { get; }
+    bool IsCanShoot { get; }
+    void Reload();
+    void Shoot();
+
+}
---
+++ Assets/Scripts/Classes/Gun/PropertyGun.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Input property gun", menuName = "Input property gun", order = 52)]
+public class InputPropertyGun : ScriptableObject
+{
+    TypeAmmo TypeAmmo;
+    int MaxAmmo;
+    float TimeReload;
+    float TimeBetweenShoot;
+    float RangeShoot;
+}
