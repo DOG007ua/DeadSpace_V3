a6d8669f67df5b8f60f263dcadcacd9b84d9365d
DOG007ua <dog007ua@gmail.com>
2021-06-21 15:30:37 +0300

    Create weapon controller

---
+++ Assets/Scripts/Classes/Gun/WeaponController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Classes.Gun
+{
+    class WeaponController : IWeaponController
+    {
+        public Gun MainGun { get; set; }
+        public Gun SecondGun { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public bool IsCanShoot => MainGun.IsCanShoot;
+
+        public void Initialize(Gun mainGun)
+        {
+            MainGun = mainGun;
+        }
+
+        public void Reload()
+        {
+            MainGun.Reload();
+        }
+
+        public void Shoot(Vector3 position)
+        {
+            MainGun.Shoot(position);
+        }
+    }
+}
