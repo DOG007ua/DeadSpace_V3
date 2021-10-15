4f64acd1731597ad141948f0245d94075a5e0f49
DOG007ua <dog007ua@gmail.com>
2021-10-15 14:57:28 +0300

    Add RedTeam parent

---
+++ Assets/Scripts/Monobeh/Unit/Red/RedTeam.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedTeam : Unit
+{
+    protected override void Initialize(GameObject gameObject)
+    {
+        base.Initialize(gameObject);
+        Team = TeamUnit.Red;
+        unitsInRange.Initialize(10, TeamUnit.Blue);
+        moveUnit = new MoveUnit(inputUnitData.property.Speed, inputUnitData.property.Heigth, transform);
+
+        listComponents.Add(moveUnit);
+    }
+}
